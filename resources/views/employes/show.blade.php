dc8895cb64fee0bfa50215055aac248250a4415c
Soufiane AJILI <soufiane.ajili@TangerAlliance.com>
2023-03-14 10:58:28 +0100

    first commit

---
+++ resources/views/employes/show.blade.php
@@ -0,0 +1,67 @@
+@extends('adminlte::page')
+
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card my-5">
+                <div class="card-header bg-white text-center p-3">
+                    <h3 class="text-dark">
+                        
+                    </h3>
+                </div>
+                <div class="card-body">
+                    
+                    <hr>
+                    <div class="form-group mb-3">
+                        <label for="fullname" class="form-label fw-bold">Full Name</label>
+                        <div class="border border-secondary rounded p-2">
+                            
+                        </div>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="fullname" class="form-label fw-bold">Matricule</label>
+                        <div class="border border-secondary rounded p-2">
+                            
+                        </div>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="fullname" class="form-label fw-bold">Departement</label>
+                        <div class="border border-secondary rounded p-2">
+                            
+                        </div>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="fullname" class="form-label fw-bold">Hired Since</label>
+                        <div class="border border-secondary rounded p-2">
+                            
+                        </div>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="fullname" class="form-label fw-bold">Phone</label>
+                        <div class="border border-secondary rounded p-2">
+                            
+                        </div>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="fullname" class="form-label fw-bold">Address</label>
+                        <div class="border border-secondary rounded p-2">
+                            
+                        </div>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="fullname" class="form-label fw-bold">City</label>
+                        <div class="border border-secondary rounded p-2">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+
+
